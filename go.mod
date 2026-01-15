22e365ac243a0b2fd28998237018d3eae2c7b386
Trojan Horse <122815686+xspassword@users.noreply.github.com>
2026-01-15 12:45:25 +0600

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/<your-user>/reconx
+
+go 1.22
+
+require (
+	github.com/spf13/cobra v1.8.0
+)
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/<your-user>/reconx/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
